61d971f748caf1af0dd6a3aacc2cf061ec03d5f1
Howard.Zuo <leftstick@qq.com>
2014-08-03 10:26:26 +0800

    disable globalstrict

--- libs/RouterParser.js
+++ libs/RouterParser.js
@@ -1,21 +1,20 @@
-'use strict';
 var express = require('express');
 var _ = require('lodash');
 
-var validate = function (obj, key, path) {
+var validate = function(obj, key, path) {
     if (!_.has(obj, key)) {
         throw new Error('miss [' + key + '] in ' + path);
     }
 };
 
-var RouterParser = function (module) {
+var RouterParser = function(module) {
     var routerJson = module.mod;
     validate(routerJson, 'when', module.path);
     validate(routerJson, 'method', module.path);
 
     var router = express.Router();
 
-    router[routerJson.method](routerJson.when, function (req, res) {
+    router[routerJson.method](routerJson.when, function(req, res) {
         res.send(routerJson.responseData);
     });
 
